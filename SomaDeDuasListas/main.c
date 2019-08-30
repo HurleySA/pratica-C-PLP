942f8ee55cd68a2be757b579d84327055a0b6473
HurleySA <matheus.eduardo.silva@ccc.ufcg.edu.br>
2019-08-30 07:08:31 -0300

    Questão Soma de Duas Listas com erro "%"

---
+++ SomaDeDuasListas/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int lista1[4];
+    int lista2[4];
+    int var, index;
+    for(int i = 0; i < 4; i++){
+        scanf("%d", %var);
+        lista1[i] = var;
+    }
+
+    for(int j = 0; j < 4; j++){
+        scanf("%d", %var);
+        lista1[j] = var;
+    }
+
+    scanf("%d", %index);
+
+    int total = lista1[index] + lista2[index];
+    printf("%d", total);
+
